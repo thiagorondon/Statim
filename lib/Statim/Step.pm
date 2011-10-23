103aae8f15b83cf200627a2aa4a6aa25cb1e9a9f
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-10-23 00:45:48 -0200

    Add initial code for new _get_step.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- lib/Statim/Step.pm
+++ lib/Statim/Step.pm
@@ -11,6 +11,18 @@ sub _calc_step {
     return floor( $epoch / $period );
 }
 
+sub _get_step {
+    my ( $self, $period, @args ) = @_;
+    foreach my $arg (@args) {
+        my ( $var, $value ) = split( /:/, $arg );
+        next unless $var eq 'step';
+        next unless $value =~ /^[0-9\-]*$/;
+        return $value;
+    }
+
+    my $dt = DateTime->now;
+    return $self->_calc_step( $period, $dt->epoch );
+}
 
 1;
 
