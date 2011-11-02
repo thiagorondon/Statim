7b6ba370a4ef67c2e9026ec26aeb795618cbe3f5
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-11-02 17:36:30 -0200

    remove unecessary code from Interface::Redis.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- lib/Statim/Storage/Interface.pm
+++ lib/Statim/Storage/Interface.pm
@@ -16,5 +16,95 @@ sub _get_period {
     return $self->conf->{$collection}->{period};
 }
 
+sub _get_counter {
+    my ( $self, $collection ) = @_;
+    return unless ref( $self->conf->{$collection}->{fields} ) eq 'HASH';
+    foreach my $field ( keys %{ $self->conf->{$collection}->{fields} } ) {
+        my $type = $self->conf->{$collection}->{fields}->{$field};
+        return $field if $type eq 'count';
+    }
+}
+
+sub _check_collection {
+    my ( $self, $collection ) = @_;
+    return 0 unless $collection;
+    return defined( $self->conf->{$collection}->{fields} ) ? 1 : 0;
+}
+
+sub _parse_args_to_add {
+    my ( $self, $collection, @args ) = @_;
+
+    my ( $counter, $incrby, %data );
+    my $declare_args = 0;
+    my @fields       = keys %{ $self->conf->{$collection}->{fields} };
+
+    foreach my $field (@fields) {
+        my $type = $self->conf->{$collection}->{fields}->{$field};
+        return '+wrong declare field type'
+          unless $type eq 'enum'
+              or $type eq 'count';    #schema error
+
+        foreach my $arg (@args) {
+            my ( $var, $value ) = split( /:/, $arg );
+            next unless $var eq $field;
+            $declare_args++;
+
+            if ( $type eq 'enum' ) {
+                $data{$field} = $value;
+            }
+            else {
+                $counter = $field;
+                $incrby  = $value;
+            }
+
+            # TODO: Add default -> schema error ?
+        }
+    }
+
+    return '+missing args' unless $declare_args == scalar(@fields);
+    return ( $counter, $incrby, %data );
+}
+
+sub _arrange_key_by_hash {
+    my ( $self, %data ) = @_;
+    my @args;
+    foreach my $item ( sort keys %data ) {
+        push( @args, $item, $data{$item} );
+    }
+    return @args;
+}
+
+# TODO: bug, we need to make sure if we dont have
+# # $counter . 'bla' field, for example.
+
+sub _parse_args_to_get {
+    my ( $self, @names ) = @_;
+    my $collection = shift(@names);
+    my $count_field = $self->_get_counter($collection) || '';
+    my ($count_to_parse) =
+      $count_field ? grep { /^$count_field/ } @names : ('');
+    my ( undef, $count_func ) =
+      $count_to_parse =~ /:/
+      ? split( ':', $count_to_parse )
+      : ( $count_field, 'sum' );
+
+    return ( $collection, $count_func,
+        grep { !/^(ts:|step:|$count_field|$count_field:)/ } @names );
+}
+
+sub _get_timestamp {
+    my ( $self, $collection, @args ) = @_;
+
+    my $has_ts   = 0;
+    my $has_step = 0;
+    foreach my $arg (@args) {
+        my ( $var, undef ) = split( /:/, $arg );
+        $has_ts   = 1 if $var eq 'ts';
+        $has_step = 1 if $var eq 'step';
+    }
+    return '+You must define only step or ts' if $has_ts and $has_step;
+    return $has_step ? $self->_get_step( $self->_get_period($collection), @args ) : $self->_get_ts(@args);
+}
+
 1;
 
--- lib/Statim/Storage/Interface/Redis.pm
+++ lib/Statim/Storage/Interface/Redis.pm
@@ -13,7 +13,6 @@ use Statim::Function;
 use base qw(Statim::Storage::Interface);
 
 # TODO: Split Storage // Schema checks // Storage::Engine.
-our $conf;
 
 sub new {
     my ( $class, $self ) = @_;
@@ -21,85 +20,9 @@ sub new {
     bless $self, $class;
 
     my $schema = Statim::Schema->new;
-    $conf = $schema->get;
+    my $conf = $schema->get;
     $self->conf($conf);
     return $self;
-}
-
-sub _get_counter {
-    my ( $self, $collection ) = @_;
-    return unless ref( $conf->{$collection}->{fields} ) eq 'HASH';
-    foreach my $field ( keys %{ $conf->{$collection}->{fields} } ) {
-        my $type = $conf->{$collection}->{fields}->{$field};
-        return $field if $type eq 'count';
-    }
-}
-
-sub _check_collection {
-    my ( $self, $collection ) = @_;
-    return 0 unless $collection;
-    return defined( $conf->{$collection}->{fields} ) ? 1 : 0;
-}
-
-sub _parse_args_to_add {
-    my ( $self, $collection, @args ) = @_;
-
-    my ( $counter, $incrby, %data );
-    my $declare_args = 0;
-    my @fields       = keys %{ $conf->{$collection}->{fields} };
-
-    foreach my $field (@fields) {
-        my $type = $conf->{$collection}->{fields}->{$field};
-        return '+wrong declare field type'
-          unless $type eq 'enum'
-              or $type eq 'count';    #schema error
-
-        foreach my $arg (@args) {
-            my ( $var, $value ) = split( /:/, $arg );
-            next unless $var eq $field;
-            $declare_args++;
-
-            if ( $type eq 'enum' ) {
-                $data{$field} = $value;
-            }
-            else {
-                $counter = $field;
-                $incrby  = $value;
-            }
-
-            # TODO: Add default -> schema error ?
-        }
-    }
-
-    return '+missing args' unless $declare_args == scalar(@fields);
-    return ( $counter, $incrby, %data );
-}
-
-sub _arrange_key_by_hash {
-    my ( $self, %data ) = @_;
-    my @args;
-    foreach my $item ( sort keys %data ) {
-        push( @args, $item, $data{$item} );
-    }
-    return @args;
-}
-
-# TODO: bug, we need to make sure if we dont have
-# $counter . 'bla' field, for example.
-
-sub _parse_args_to_get {
-    my ( $self, @names ) = @_;
-    my $collection = shift(@names);
-    my $count_field = $self->_get_counter($collection) || '';
-    my ($count_to_parse) =
-      $count_field ? grep { /^$count_field/ } @names : ('');
-    my ( undef, $count_func ) =
-      $count_to_parse =~ /:/
-      ? split( ':', $count_to_parse )
-      : ( $count_field, 'sum' );
-
-    return ( $collection, $count_func,
-        grep { !/^(ts:|step:|$count_field|$count_field:)/ } @names );
 }
 
 sub _make_key_name {
@@ -130,7 +53,7 @@ sub add {
     my @keys = $self->_arrange_key_by_hash(%data);
     my $key = $self->_make_key_name( $collection, $period, @keys );
 
-    my $aggregate = $conf->{$collection}->{aggregate};
+    my $aggregate = $self->conf->{$collection}->{aggregate};
     return $self->_save_data( $key, $incrby, $aggregate );
 }
 
@@ -167,11 +90,11 @@ sub _get_key_value {
 sub _get_all_possible_keys {
     my ( $self, $collection, $ts, $argr ) = @_;
 
-    my @fields = keys %{ $conf->{$collection}->{fields} };
+    my @fields = keys %{ $self->conf->{$collection}->{fields} };
     my @ns;
 
     foreach my $item ( sort @fields ) {
-        my $type = $conf->{$collection}->{fields}->{$item};
+        my $type = $self->conf->{$collection}->{fields}->{$item};
         next if $type eq 'count';
 
         my ($has_item) = grep { /^$item:/ } @{$argr};
@@ -195,21 +118,6 @@ sub _get_all_possible_keys {
     return [@ps];
 }
 
-sub _get_timestamp {
-    my ( $self, $collection, @args ) = @_;
-
-    my $has_ts   = 0;
-    my $has_step = 0;
-    foreach my $arg (@args) {
-        my ( $var, undef ) = split( /:/, $arg );
-        $has_ts   = 1 if $var eq 'ts';
-        $has_step = 1 if $var eq 'step';
-    }
-    return '+You must define only step or ts' if $has_ts and $has_step;
-    return $has_step ? 
-      $self->_get_step( $self->_get_period($collection), @args ) : $self->_get_ts(@args);
-}
-
 sub get {
     my ( $self, @args ) = @_;
     my ( $collection, $count_func, @names ) = $self->_parse_args_to_get(@args);
@@ -226,7 +134,7 @@ sub get {
             ts_args    => [@ts_args],
             names      => [@names],
             storage    => $self,
-            conf       => $conf
+            conf       => $self->conf
         }
     );
 
